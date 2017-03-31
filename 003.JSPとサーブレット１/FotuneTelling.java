d10f52d0fa36017fd9538d58c68f7a291348c4ee
DESKTOP-P6G1HD0\takuro <bijinesu1018@gmail.com>
2017-03-31 17:42:58 +0900

    占いシステムの作成
    「JSPとサーブレット1」のスライドで解説した、占いシステムを実際に作成してください。

---
+++ 003.JSPとサーブレット１/FotuneTelling.java
@@ -0,0 +1,99 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package jp.geek.job.camp;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.util.Random;
+/**
+ *
+ * @author takuro
+ */
+public class FotuneTelling extends HttpServlet {
+
+    /**
+     * Processes requests for both HTTP <code>GET</code> and <code>POST</code>
+     * methods.
+     *
+     * @param request servlet request
+     * @param response servlet response
+     * @throws ServletException if a servlet-specific error occurs
+     * @throws IOException if an I/O error occurs
+     */
+    protected void processRequest(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        response.setContentType("text/html;charset=UTF-8");
+        PrintWriter out = response.getWriter();
+        //乱数の配列を作成
+        String lucklist[] = {"大吉","中吉","末吉","吉","凶",};
+        
+        //乱数のクラスを生成
+        Random rand =new Random();
+        
+        //nextIntメソッドで乱数(rand)を取得する。
+        Integer index = rand.nextInt(lucklist.length);
+        
+        //<body>の中で出力をする。[index]が乱数の為、luckListの配列内がランダムで表示される。
+        try{
+            /* TODO output your page here. You may use following sample code. */
+            out.println("<!DOCTYPE html>");
+            out.println("<html>");
+            out.println("<head>");
+            out.println("<title>本日の運勢占い</title>");            
+            out.println("</head>");
+            out.println("<body>");
+            out.println("<h1>" + "今日のあなたの運勢は…" + lucklist[index] + "!" + "</h1>");
+            out.println("</body>");
+            out.println("</html>");
+        }finally{
+            out.close();
+        }
+    }
+
+    // <editor-fold defaultstate="collapsed" desc="HttpServlet methods. Click on the + sign on the left to edit the code.">
+    /**
+     * Handles the HTTP <code>GET</code> method.
+     *
+     * @param request servlet request
+     * @param response servlet response
+     * @throws ServletException if a servlet-specific error occurs
+     * @throws IOException if an I/O error occurs
+     */
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        processRequest(request, response);
+    }
+
+    /**
+     * Handles the HTTP <code>POST</code> method.
+     *
+     * @param request servlet request
+     * @param response servlet response
+     * @throws ServletException if a servlet-specific error occurs
+     * @throws IOException if an I/O error occurs
+     */
+    @Override
+    protected void doPost(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        processRequest(request, response);
+    }
+
+    /**
+     * Returns a short description of the servlet.
+     *
+     * @return a String containing servlet description
+     */
+    @Override
+    public String getServletInfo() {
+        return "Short description";
+    }// </editor-fold>
+
+}
